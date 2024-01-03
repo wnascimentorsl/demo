1c96a57a5fb9c290c55f94635b3f6913e920a3fc
wnascimentorsl <plagiador.rsl@gmail.com>
2024-01-02 23:08:11 -0300

    Create Bank

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
-require 'functions.php';
+require "functions.php";
 
-require 'router.php';
+$dsn = "pgsql:host=localhost;port=5432;user='postgres';password='postgres';dbname=demo";
+
+$pdo = new PDO($dsn); //data
+
+$statement = $pdo->prepare("select * from posts");
+$statement -> execute();
+
+$posts = $statement->fetchAll();
+
+dd($posts);
