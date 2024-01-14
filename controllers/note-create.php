294714b4ad51bc7f88d10a0dc74e82c95da8bf19
wnascimentorsl <plagiador.rsl@gmail.com>
2024-01-14 18:37:42 -0300

    changes

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'Validator.php';
+
 $config = require('config.php');
 $db = new Database($config['database']);
 
@@ -8,12 +10,9 @@ $heading = 'Create Note';
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
     $errors = [];
 
-    if (strlen($_POST['body']) === 0){
-        $errors['body'] = 'A body is required';
-    }
 
-    if (strlen($_POST['body']) > 300){
-        $errors['body'] = 'The body must contain less than 300 characters';
+    if (! Validator::string($_POST['body'], 1, 350)){
+        $errors['body'] = 'A body of no more than 350 characters is required';
     }
 
     if (empty($errors)){
